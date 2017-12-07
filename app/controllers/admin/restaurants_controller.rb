d27984b9b239ec991ae02d4749959b7bbb6f69e2
badbug wne <sweewen45@gmail.com>
2017-12-07 14:07:24 +0800

    copy views and restaurant controller action form origin project

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,52 @@
 class Admin::RestaurantsController < ApplicationController
+before_action :authenticate_user!
+before_action :authenticate_admin
+before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
-    @restaurants = Restaurant.all
+    @restaurants = Restaurant.page(params[:page]).per(10)
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "Restaurant was successfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "Restaurant was dailed to create"
+      render :new
+    end  
+  end
+ 
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to admin_restaurants_path(@restaurant)
+      flash[:notice] = "Restaurant was successfully updated"
+    else
+      flash.now[:alert] = "Restaurant was failed to update"
+      render :edit
+    end  
+  end 
+
+  def destroy
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+    flash[:alert] = "餐廳已刪除"
+  end 
+    
+private  
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :descrition, :image) 
+  end  
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end  
+
 end
+
