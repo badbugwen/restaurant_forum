cb84d07e812c6a101a73ec5af88387c1fb3c6da6
badbug wne <sweewen45@gmail.com>
2018-01-02 15:34:57 +0800

    add user update action and edit view, add update to user route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+  before_action :set_user
+
+  def show   
   end
+
+
+  def update
+    if @user == current_user
+      @user.update(user_params)
+      redirect_to user_path(params[:id])
+      flash[:notice] = "User's profile was successfully updated"
+    else
+      flash.now[:alert] = "User's profile was failed to update"
+      render :edit
+    end
+  end
+
+
+
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+    params.require(:user).permit(:name, :intro, :avatar)
+    end
+
 end
