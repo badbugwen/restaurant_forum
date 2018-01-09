1d8c3fd4e819ece0937d9baf1670ff8c043cec0d
badbug wne <sweewen45@gmail.com>
2018-01-09 13:49:51 +0800

    add dependency between models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   def index
   end
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :category, optional: true
 end
