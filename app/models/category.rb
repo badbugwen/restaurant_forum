e881b597fac438caa31f8df006221cc1120d1ea0
badbug wne <sweewen45@gmail.com>
2018-01-09 22:02:58 +0800

    add comments in models(category, restaurant and user)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates_presence_of :name
 
+  # 如果該類別已經有了餐廳，就不允許刪除類別（刪除時拋出 Error）
   has_many :restaurants, dependent: :restrict_with_error
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :comments, dependent: :destroy
-  has_many :restaurants, through: :comments
-
-  has_many :favorites, dependent: :destroy
-  has_many :favorited_restaurants, through: :favorites, source: :restaurant
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :name
 
   mount_uploader :avatar, AvatarUploader
 
+  # 「使用者評論很多餐廳」的多對多關聯
+  # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
+  has_many :comments, dependent: :restrict_with_error
+  has_many :restaurants, through: :comments
+
+  # 「餐廳被很多使用者收藏」的多對多關聯
+  #當 Restaurant 物件被刪除時，順便刪除依賴的 favorite
+  has_many :favorites, dependent: :destroy
+  # 將關聯名稱自訂為 :favorited_users
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
+  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin" 
   end         
