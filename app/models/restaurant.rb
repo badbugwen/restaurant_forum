d85b3f9e039f3a5e776812f7cb263e2a3a231c9c
badbug wne <sweewen45@gmail.com>
2018-01-10 12:11:28 +0800

    add is_favorited? method to restaurant model for checking favorite / unfavorite button(Turn the button to partial:shared/favorite so that can be used in index, too)

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -13,4 +13,9 @@ class Restaurant < ApplicationRecord
   # 將關聯名稱自訂為 :favorited_users
   # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
   has_many :favorited_users, through: :favorites, source: :user
+
+  # 判斷是否有user收藏此餐廳
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
 end
