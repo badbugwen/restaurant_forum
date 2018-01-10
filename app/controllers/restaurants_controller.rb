5f1db1b64a8810ceae624786798b716852070b85
badbug wne <sweewen45@gmail.com>
2018-01-10 11:20:53 +0800

    add favorite / unfavorite action on RestaurantsController. Fix typo (usr>user) in favorite model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,12 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only:[:show, :dashboard, :favorite, :unfavorite]
+
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
     @categories = Category.all
   end
   
   def show
-    @restaurant = Restaurant.find(params[:id])
     @comment = Comment.new
   end
 
@@ -16,8 +17,27 @@ class RestaurantsController < ApplicationController
   end
 
   def dashboard
-    @restaurant = Restaurant.find(params[:id])
   end
   
+   # POST /restaurants/:id/favorite
+  def favorite
+    @restaurant.favorites.create!(user: current_user)
+    # 導回上一頁，若找不到上一頁則回首頁
+    redirect_back(fallback_location: root_path)
+  end
+
+  # POST /restaurants/:id/unfavorite
+  def unfavorite
+    favorite = Favorite.where(restaurant: @restaurant, user: current_user)
+    favorite.destroy_all
+    redirect_back(fallback_location: root_path)
+  end  
+
+  ###################
+  private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
 
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,4 @@
 class Favorite < ApplicationRecord
-  belongs_to :usr
+  belongs_to :user
   belongs_to :restaurant
 end
