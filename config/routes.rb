ff0fa972aa211e1634c2f319bad6d39e94926262
badbug wne <sweewen45@gmail.com>
2018-01-10 09:15:08 +0800

    setup routes for favorite / unfavorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  
+  # use devise gem for user login
   devise_for :user, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update]
 
+  # user僅開放觀看 index & show for restaurant
   resources :restaurants, only: [:index, :show] do
+    # 在restaurant下可Create and Destroy comments
+    # POST restaurants/restaurant_id/comments
+    # DELETE restaurants/restaurant_id/comments/:id
     resources :comments, only: [:create, :destroy]
 
     # 瀏覽所有餐廳的最新動態
@@ -12,21 +16,25 @@ Rails.application.routes.draw do
       get :feeds
     end
     
-     # 瀏覽個別餐廳的 Dashboard
     member do
+      # 瀏覽個別餐廳的 Dashboard
+      # GET restaurants/:id/dashboard
       get :dashboard
-    end  
 
+      # 加入最愛/取消最愛個別餐廳
+      post :favorite
+      post :unfavorite
+    end  
   end
 
+  # 一般使用者僅開放Read for category
   resources :categories, only: [:show]
   root "restaurants#index"
   
+  # admin開放CRUD for category
   namespace :admin do
     resources :restaurants
     resources :categories
     root "restaurants#index"
   end
-  
-
 end
