0497a07e754a4c35897dc2f5dfa73c612b0f73e8
badbug wne <sweewen45@gmail.com>
2018-01-25 15:22:52 +0800

    fix SyntaxError in firendship & followshop controller

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -2,7 +2,7 @@ class FollowshipsController < ApplicationController
   def create
     @followship = current_user.followships.build(following_id: params[:following_id])
     if @followship.save
-      redirect_back(fallback_location: root_path), notice: "您已將 #{User.find_by(id: params[:following_id]).name} 加入追蹤名單!"    
+      redirect_back(fallback_location: root_path, notice: "您已將 #{User.find_by(id: params[:following_id]).name} 加入追蹤名單!" )   
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
